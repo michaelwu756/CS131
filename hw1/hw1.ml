13e38465831a3ed6f7d36fd1fb6823c93fbe1793
Michael Wu <cheeserules43@gmail.com>
2018-04-05 09:24:11 -0700

    implemented equal_sets

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -3,3 +3,12 @@ let rec subset a b =
   | [] -> true
   | f::s -> if List.mem f b then subset s b else false
 ;;
+
+let rec equal_sets a b =
+  if List.length a = 0 && List.length b = 0 then true
+  else if List.length a = 0 && List.length b <> 0 then false
+  else if List.length b = 0 && List.length a <> 0 then false
+  else if not (List.mem (List.hd a) b) then false
+  else let removeElement l = List.filter (fun x -> x <> List.hd a) l in
+       equal_sets (removeElement a) (removeElement b)
+;;
