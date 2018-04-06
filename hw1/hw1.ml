88689877e7b2f7ae9d17284da5d4533a5daf870d
Michael Wu <cheeserules43@gmail.com>
2018-04-05 21:45:11 -0700

    implemented fixed and periodic points

--- hw1/hw1.ml
+++ hw1/hw1.ml
@@ -10,9 +10,11 @@ let rec subset a b =
 ;;
 
 let rec equal_sets a b =
-  if List.length a = 0 && List.length b = 0 then true
-  else if List.length a = 0 && List.length b <> 0 then false
-  else if List.length b = 0 && List.length a <> 0 then false
+  let aLen = List.length a in
+  let bLen = List.length b in
+  if aLen = 0 && bLen = 0 then true
+  else if aLen = 0 && bLen <> 0 then false
+  else if bLen = 0 && aLen <> 0 then false
   else if not (List.mem (List.hd a) b) then false
   else let removeElement l = List.filter (fun x -> x <> List.hd a) l in
        equal_sets (removeElement a) (removeElement b)
@@ -40,4 +42,20 @@ let rec set_diff a b =
   | f::s -> if not (List.mem f b) && not (List.mem f s)
             then f::set_diff s b
             else set_diff s b
+;;
+
+let rec computed_fixed_point eq f x =
+  let applied = f x in
+  if eq applied x
+  then x
+  else computed_fixed_point eq f applied
+;;
+
+let computed_periodic_point eq f p x =
+  let rec computed_periodic_point_helper eq f p x l =
+    let len = List.length l in
+    let appended = x::l in
+    if len >= p && eq (List.hd appended) (List.nth appended p) then x
+    else computed_periodic_point_helper eq f p (f x) appended in
+  computed_periodic_point_helper eq f p x []
 ;;
