89013f36fba174efc823687d1b6c4d12203e0084
Michael Wu <cheeserules43@gmail.com>
2018-05-12 14:19:40 -0700

    indented code

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -4,12 +4,12 @@ transpose([F|Fs], Ts) :-
 
 transpose([], _, []).
 transpose([_|Rs], Ms, [Ts|Tss]) :-
-        lists_firsts_rests(Ms, Ts, Ms1),
-        transpose(Rs, Ms1, Tss).
+    lists_firsts_rests(Ms, Ts, Ms1),
+    transpose(Rs, Ms1, Tss).
 
 lists_firsts_rests([], [], []).
 lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
-        lists_firsts_rests(Rest, Fs, Oss).
+    lists_firsts_rests(Rest, Fs, Oss).
 
 append([L], L).
 append([H|T], L) :-
