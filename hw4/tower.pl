c5f1c39d9f762cd80403e25f81f0d31b7e26554f
Michael Wu <cheeserules43@gmail.com>
2018-05-12 07:06:37 -0700

    converted sodoku code

--- hw4/tower.pl
+++ hw4/tower.pl
@@ -1,67 +1,32 @@
 tower(N, T, C) :- true.
 
-values(L, MaxVal) :- fd_domain(Dom, 1, MaxVal), fd_dom(Dom, L).
-row(R, MaxVal) :- values(M, MaxVal), permutation(M, R).
-board([], _).
-board(_, _).
+transpose([], []).
+transpose([F|Fs], Ts) :-
+    transpose(F, [F|Fs], Ts).
 
-noattack(Q,[],_).
-noattack(Q1,[Q2|Rest],Diff):-
-    Q1#\=#Q2,
-    Diff#\=#Q1-Q2,
-    Diff#\=#Q2-Q1,
-    Diff2 is Diff-1,
-    noattack(Q1,Rest,Diff2).
-safe([]).
-safe([Q|T]):-noattack(Q,T,1),safe(T).
+transpose([], _, []).
+transpose([_|Rs], Ms, [Ts|Tss]) :-
+        lists_firsts_rests(Ms, Ts, Ms1),
+        transpose(Rs, Ms1, Tss).
 
-eightqueens(Solution):-
-    Solution = [_,_,_,_,_,_,_,_],
-    fd_domain(Solution,1,8),
-    safe(Solution),
-    fd_labeling(Solution).
+lists_firsts_rests([], [], []).
+lists_firsts_rests([[F|Os]|Rest], [F|Fs], [Os|Oss]) :-
+        lists_firsts_rests(Rest, Fs, Oss).
 
+append([L], L).
+append([H|T], L) :-
+    append(T, LSub),
+    append(H, LSub, L).
 
-list_of_length(L, 0) :- L=[].
-list_of_length(_, -1) :- !, fail.
-list_of_length(L, N) :-
-    Dec is N-1,
-    L=[_|Tail],
-    list_of_length(Tail, Dec).
+same_length([], []).
+same_length([_|T1], [_|T2]) :- same_length(T1, T2).
 
-board_of_size(B, 0, _) :- B=[].
-board_of_size(_, -1, _) :- !, fail.
-board_of_size(B, Row, Col) :-
-    Dec is Row-1,
-    list_of_length(L, Col),
-    B=[L|Tail],
-    board_of_size(Tail, Dec, Col).
-board_of_size(B, N) :- board_of_size(B, N, N).
-
-board_domain([], _).
-board_domain([H|T], MaxVal) :-
-    fd_domain(H, 1, MaxVal),
-    fd_all_different(H),
-    board_domain(T, MaxVal).
-
-board_labeling([]).
-board_labeling([H|T]) :-
-    fd_labeling(H),
-    board_labeling(T).
-
-check_rows_differ([], []).
-check_rows_differ([HL|TL], [HR|TR]) :-
-    \+(HL=HR),
-    check_rows_differ(TL, TR).
-
-check_columns([_]).
-check_columns([H,I|T]) :-
-    check_rows_differ(H, I),
-    check_columns([I|T]),
-    check_columns([H|T]).
-
-permutationsFD(Sol, MaxVal) :-
-    board_of_size(Sol, MaxVal),
-    board_domain(Sol, MaxVal),
-    board_labeling(Sol),
-    check_columns(Sol).
+towers(N, T) :-
+    length(T, N),
+    maplist(same_length(T), T),
+    append(T, Vs),
+    fd_domain(Vs, 1, N),
+    maplist(fd_all_different, T),
+    transpose(T, Trans),
+    maplist(fd_all_different, Trans),
+    fd_labeling(Vs).
